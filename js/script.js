4f3460105564c2d9befdd5143878d7684785195a
ana <aninhaa@exemplo.br>
2023-05-18 17:30:41 -0300

    exibindo pontuacao final

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const mario = document.querySelector('.mario')/*retorna o primeiro Element dentro do documento que corresponde ao seletor.Se nada for encontrado será retornado null*/
 const pipe = document.querySelector('.pipe')
+const clouds = document.querySelector('.clouds')
 const gameover = document.querySelector('.gameover')
 const botaovoltar = document.querySelector('.btn')
 const tempo = document.getElementById('seconds')
 const pontos = document.getElementById('points')
 const nivel = document.getElementById('level')
 const botaoinicio = document.querySelector('.botaoIniciar')
-const pontuacaofinal = 0
+const pontuacaofinal = document.querySelector('.pontuacaofinal')
+const nivelfim = document.querySelector('.nivelfim')
 let jumps = 0;
 let level = 1;
 let points = 0;
@@ -17,9 +19,8 @@ let initialPos = -80;
 
 const counter = setInterval(() => {
     initialTime++;
-    document.getElementById('seconds').textContent = 'Tempo:  ' + initialTime;    
+    document.getElementById('seconds').textContent = 'Tempo:  ' + initialTime;
 }, 1000);
-    
 
 const jump = () => {
     const map = [
@@ -47,8 +48,20 @@ const jump = () => {
         document.getElementById('points').textContent = 'Pontos:  ' + points;
     }, 400)//após 500milissegundos, a função será executada
 }
-
+//exibindo pontuacao e nivel final
+function pontuacaoFinal() {
+    if (points <= 0) {
+        pontuacaofinal.textContent = 'Voce nao tem pontos'
+    }
+    else {
+        pontuacaofinal.textContent = 'Pontuacao Final: ' + points
+    }
+}
+function nivelFim() {
+    nivelfim.textContent = 'Nivel: ' + nivel
+}
 const loop = setInterval(() => {
+
     const pipePosition = pipe.offsetLeft//deslocamento esquerdo
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')//estilo computado na imagem do mário
     //+ - define um valor numérico
@@ -64,19 +77,14 @@ const loop = setInterval(() => {
         mario.src = 'img/game-over.png' //insercao da img de gameover
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
+        clouds.style.display = 'none'
         tempo.style.display = 'none'
         pontos.style.display = 'none'
         nivel.style.display = 'none'
-        var divGameOverImg = document.createElement('div')
-        divGameOverImg.classList = 'd-flex'
-        divGameOverImg.classList = 'justify-content-center'
-        var imgGameOver = document.createElement('img')
-        imgGameOver.classList = 'gameover'
-        
         gameover.src = 'img/gameover-text.png'
         botaovoltar.innerHTML = 'Voltar'
-        
-        //FORMATACOES IMG GAMEOVER
+        pontuacaofinal = pontuacaoFinal()
+        //FORMATACOES GAMEOVER
 
         clearInterval(loop) //parar aplicacao    para nao ser um loop infinito
         clearInterval(counter);
@@ -87,10 +95,9 @@ const loop = setInterval(() => {
 setInterval(() => {
     initialPos = initialPos + (15 + level);
     pipe.style.right = `${initialPos}px`;
-    if(initialPos > window.innerWidth + 100){
+    if (initialPos > window.innerWidth + 100) {
         initialPos = -80;
     }
 }, 17);
 
 document.addEventListener('keydown', jump)//Adicione um evento click ao documento
- 
